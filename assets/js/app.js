d8eedae97e8b8a9ab7c057cc23ed18b0485192cd
Sujata Gore <goress342@gmail.com>
2024-05-08 12:35:49 +0530

    Food Blog

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+var cl = console.log;
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
